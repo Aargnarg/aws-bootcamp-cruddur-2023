192fdc9813020305f673272e9d47a408cba6c607
Aargnarg <samuelwitt.home@gmail.com>
2023-03-04 08:03:12 +0000

    xray is now working

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -26,10 +26,6 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
-#AWS-XRAY-SDK -------------------------------------------
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 # HoneyComb -------------------------------------------
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -40,6 +36,11 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+#AWS-XRAY-SDK -------------------------------------------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb -------------------------------------------
 # Initialize automatic instrumentation with Flask
