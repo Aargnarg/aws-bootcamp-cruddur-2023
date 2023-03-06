9af37a12a1bdfa5d2f28186a6a4be54c64a5ae52
Aargnarg <samuelwitt.home@gmail.com>
2023-03-06 05:13:06 +0000

    add rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,6 +31,14 @@ import watchtower
 import logging
 from time import strftime
 
+
+#Rollbar
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
+
 # Configuring Logger to Use CloudWatch
 #LOGGER = logging.getLogger(__name__)
 #LOGGER.setLevel(logging.DEBUG)
@@ -49,6 +57,27 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+
+#Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+
 
 #AWS-XRAY-SDK -------------------------------------------
 #xray_url = os.getenv("AWS_XRAY_URL")
@@ -86,6 +115,10 @@ def data_message_groups():
 #    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #    return response
 
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
@@ -115,7 +148,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run(logger = LOGGER)
+  data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
