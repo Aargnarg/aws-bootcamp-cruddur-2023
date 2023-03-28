bd7f4f656b677f32d4a5331d05211802c5a4e107
Aargnarg <samuelwitt.home@gmail.com>
2023-03-28 07:17:35 +0000

    hard coded seed to get activities to work

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Andrew Brown', 'andrewbrown' , 'fake.email@work.com', 'MOCK'),
+  ('Andrew Bayko', 'bayko' , 'faker.emailier@workiest.com', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
